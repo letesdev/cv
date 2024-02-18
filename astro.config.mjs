3f3456dd452fa215a6217d1cb4ebdef11b301598
carlos.santos-seisdedos <carlos.santos-seisdedos@renault.com>
2024-02-18 12:49:22 +0100

    prueba deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://letesdev.github.io',
+    base: '/cv',
+});
