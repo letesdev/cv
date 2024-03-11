fb5008fbda1137e39139f37bc3dd0d26d8ee5d86
carlos.santos-seisdedos <carlos.santos-seisdedos@renault.com>
2024-03-11 21:09:24 +0100

    Added some SEO recommendations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://letesdev.github.io',
-    base: '/cv',
+    base: '/portfolio',
     server: { host: true },
 });
